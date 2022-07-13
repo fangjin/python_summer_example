16f8104a7c7b6f183b9f0eaa9d7642c464c512c4
KingWeeb <iuhgpvz@hotmail.com>
2022-07-13 16:15:09 -0400

    Add files via upload

---
+++ L12_nested_non_Func.py
@@ -0,0 +1,9 @@
+outer_loop_list = [0, 1]
+inner_loop_list = ["a", "b"]
+
+
+for each_outer_iteration in [0, 1]:
+    print("\n outer loop iteration ", each_outer_iteration, "\n")
+
+    for each_inner_iteration in ["a", "b"]:
+        print("    inner loop iteration ", each_inner_iteration)
